ebbbd193763aa80a405bf0e7c5f6e9196953d9ae
Anshpandey9 <itsanshpandey8@gmail.com>
2024-10-21 10:25:52 +0530

    Add files via upload
    This is a task -1 of CodSoft internship program of java programming.

---
+++ numberguessing.java
@@ -0,0 +1,9 @@
+import java.util.random;
+import java.util.*;
+public class numberguessing{
+    public static void main(String[] args) {
+        Random random = new Random()
+        num = random.nextInt(100);
+        System.out.println(num); 
+    }
+}
